a7186ac0e229c249745deef3d519f3c9a48b67b5
Tim Lee <tlee87@gmail.com>
2022-09-03 00:42:17 -0400

    style fixes, add buy and sell on chart

--- components/SwapChart.tsx
+++ components/SwapChart.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import Moment from 'moment-timezone'
 import CanvasJSReact from '../canvasjs.react'
-var CanvasJS = CanvasJSReact.CanvasJS
 var CanvasJSChart = CanvasJSReact.CanvasJSChart
 
 import swapRecordService from '../services/swapRecord.service'
@@ -14,62 +13,99 @@ interface DataPoint {
 }
 
 const SwapChart = ({ swapRule }: { swapRule: ISwapRule | undefined }) => {
+  const [dataLoaded, setDataLoaded] = useState(false)
   const [buyDataPoints, setBuyDataPoints] = useState([] as DataPoint[])
   const [sellDataPoints, setSellDataPoints] = useState([] as DataPoint[])
+  const [buyTargetPoints, setBuyTargetPoints] = useState([] as DataPoint[])
+  const [sellTargetPoints, setSellTargetPoints] = useState([] as DataPoint[])
+
 
   useEffect(() => {
-    if ( !!swapRule?._id ) {
-      onLoadSwapRecords()
-    }
-  }, [swapRule])
+    onLoadSwapRecords()
+  }, [])
 
   const onLoadSwapRecords = async () => {
     if ( !swapRule ) {
       return
     }
+    setDataLoaded(false)
     const swapRecords = await swapRecordService.getSwapRecords(swapRule._id)
     if ( swapRecords ) {
-      setBuyDataPoints(swapRecords.filter( record => record.inputTokenSymbol === swapRule.baseToken.symbol ).map( record => ({
-        label: 'Buy',
+      const newBuyDataPts = swapRecords.filter( record => record.inputTokenSymbol === swapRule.baseToken.symbol ).map( record => ({
+        label: 'Buy Price',
         x: Moment(record.timestamp).toDate(),
         y: record.unitPrice,
-      })))
-      setSellDataPoints(swapRecords.filter( record => record.inputTokenSymbol !== swapRule.baseToken.symbol ).map( record => ({
-        label: 'Sell',
+      }))
+      const newSellDataPts = swapRecords.filter( record => record.inputTokenSymbol !== swapRule.baseToken.symbol ).map( record => ({
+        label: 'Sell Price',
         x: Moment(record.timestamp).toDate(),
         y: record.unitPrice,
-      })))
+      }))
+      setBuyDataPoints(newBuyDataPts)
+      setSellDataPoints(newSellDataPts)
+      const biggerDataSet = newBuyDataPts.length > newSellDataPts.length ? newBuyDataPts : newSellDataPts
+      if ( swapRule.baseInput ) {
+        const newBuyTargetPoints = biggerDataSet.map( point => ({
+          label: 'Buy Target',
+          x: point.x,
+          y: swapRule.swapTarget,
+        }))
+        setBuyTargetPoints(newBuyTargetPoints)
+      }
+      if ( swapRule.swapInput ) {
+        const newSellTargetPoints = biggerDataSet.map( point => ({
+          label: 'Sell Target',
+          x: point.x,
+          y: swapRule.baseTarget,
+        }))
+        setSellTargetPoints(newSellTargetPoints)
+      }
     }
+    setDataLoaded(true)
   }
 
   return(
     <div>
-      <CanvasJSChart
+      { dataLoaded &&
+        <CanvasJSChart
           options={{
-          theme: "light2",
-          title: {
-            text: `${ swapRule?.swapToken?.symbol || '?' } Prices`
-          },
-          axisY: {
-            title: `${ swapRule?.swapToken?.symbol || '?' } Price in ${ swapRule?.baseToken?.symbol || '?' }`,
-            prefix: "$",
-          },
-          data: [
-            {
-              type: "line",
-              xValueFormatString: "MM hh:mm",
-              yValueFormatString: "$#,##0.00",
-              dataPoints: buyDataPoints,
+            theme: "light2",
+            title: {
+              text: `${ swapRule?.swapToken?.symbol || '?' } Prices`
             },
-            {
-              type: "line",
-              xValueFormatString: "MM hh:mm",
-              yValueFormatString: "$#,##0.00",
-              dataPoints: sellDataPoints,
+            axisY: {
+              title: `${ swapRule?.swapToken?.symbol || '?' } Price in ${ swapRule?.baseToken?.symbol || '?' }`,
+              prefix: "$",
             },
-          ]
-        }}
-      />
+            data: [
+              {
+                type: "line",
+                xValueFormatString: "MM hh:mm",
+                yValueFormatString: "$#,##0.00",
+                dataPoints: buyDataPoints,
+              },
+              {
+                type: "line",
+                xValueFormatString: "MM hh:mm",
+                yValueFormatString: "$#,##0.00",
+                dataPoints: sellDataPoints,
+              },
+              {
+                type: "line",
+                xValueFormatString: "MM hh:mm",
+                yValueFormatString: "$#,##0.00",
+                dataPoints: buyTargetPoints,
+              },
+              {
+                type: "line",
+                xValueFormatString: "MM hh:mm",
+                yValueFormatString: "$#,##0.00",
+                dataPoints: sellTargetPoints,
+              },
+            ]
+          }}
+        />
+      }
     </div>
   )
 }
